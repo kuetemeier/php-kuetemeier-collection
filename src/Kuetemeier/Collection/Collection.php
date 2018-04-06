9e938d5827f07ffe91fd41845232921e0a48ec82
Joerg Kuetemeier <jk@kuetemeier.net>
2018-04-06 19:30:54 +0200

    Adds initial methods and Tests.

--- src/Kuetemeier/Collection/Collection.php
+++ src/Kuetemeier/Collection/Collection.php
@@ -26,19 +26,264 @@
 namespace Kuetemeier\Collection;
 
 /**
-  * A common Collection PHP class to use as a baseline in your projects.
-  *
-  * @author Jörg Kütemeier
-  * @since 0.1.0
-  */
+ * A common Collection PHP class to use as a baseline in your projects.
+ *
+ * This Collection acts like a Hash or Map. It stores its elements based
+ * on a `string` key. The key may have multiple levels divided by a slash `/`
+ * in the string.
+ *
+ * @author Jörg Kütemeier
+ * @since 0.1.0
+ */
 class Collection
 {
-    public function __construct()
+
+    /** @var array Holds registered elements. */
+    protected $elements = array();
+
+    public function __construct($initValues = null)
     {
+        if (!isset($initValues)) {
+            return;
+        }
+
+        if (is_array($initValues)) {
+            $this->elements = $initValues;
+        } elseif (is_string($initValues)) {
+            $this->loadFromJSON($initValues);
+        }
     }
 
-    public function helloWorld()
+
+    public static function fromJSON($filename)
     {
-        echo "Hallo Welt - Hello World!";
+        return new Collection($filename);
+    }
+
+
+    public function loadFromJSON($filename)
+    {
+        $json = json_decode(file_get_contents($path), true);
+
+        $this->elements = $json ?: false;
+    }
+
+
+    /**
+     * Retrieve an element via its provided key.
+     *
+     * **HINT** Performance considerations - PHP uses copy on write, passing a value is ALWAYS faster, than passing
+     * a reference by ourself (PHP needs overhead for that). So use `get` not `getRef` in
+     * normal situations.
+     *
+     * **NOTE** We are not trimming keys.
+     *
+     * @param string $ukey    (optional) Unique element key. If none is given, return all elements.
+     * @param mixed  $default Default value to return if element does not exist (key not found).
+     *
+     * @return mixed Stored element or $default value.
+     *
+     * @since 0.1.0
+     * @see Collection::getRef() Get a reference of an element.
+     * @see http://www.phpinternalsbook.com/zvals/memory_management.html Copy on Write in PHP
+     */
+    public function get($ukey = null, $default = null)
+    {
+        if (!isset($ukey)) {
+            return $this->elements;
+        }
+
+        $element = $this->elements;
+
+        foreach (explode('/', $ukey) as $key) {
+            if (!isset($element[$key])) {
+                return $default;
+            }
+            $element = $element[$key];
+        }
+
+        return $element;
+    }
+
+
+    /**
+     * Retrieve a reference to an element via its provided key.
+     *
+     * **WARNING:** With a reference, you will directly change the representation of
+     *          the data in this collection. But that was exactly what you was
+     *          expecting, right? ;-)
+     *
+     * **IMPORTANT:** Remember to call it like this (with extra `&`):
+     * ```
+     * // init a collection with some values
+     * $c = new Collection(self::TEST_ARRAY);
+     *
+     * // get a reference of an element
+     * $e = &$c->getRef('some/key');
+     * ```
+     * Do you noticed the extra `&`, when we call the method?
+     * If we would just use `$e = $c->getRef('some/key')` the equal operator
+     * would create a COPY and not a REFERENCE.
+     *
+     * P.S.
+     * There is no `$default` as in `Collection::get()`. If we cannot find the key,
+     * we cannot pass a reference. It will return `null`.
+     *
+     * @param string $ukey    (optional) Unique element key. If none is given, return all elements.
+     *
+     * @return mixed Stored element or null if no element is found.
+     *
+     * @since 0.1.0
+     * @see Collection::get() Get a copy of an element.
+     */
+    public function &getRef($ukey = null)
+    {
+        if (!isset($ukey)) {
+            return $this->elements;
+        }
+
+        $element = &$this->elements;
+
+        foreach (explode('/', $ukey) as $key) {
+            if (!isset($element[$key])) {
+                return null;
+            }
+            $element = &$element[$key];
+        }
+
+        return $element;
+    }
+
+
+    /**
+     * Store an element into this collection, bound to a specified key.
+     *
+     * @param string $ukey  Unique element key.
+     * @param mixed  $value The Element to store.
+     *
+     * @return bool True on successfull storage, false otherwise
+     *
+     * @since 0.1.0
+     */
+    public function set($ukey, $value, $override = false)
+    {
+        if (!isset($ukey)) {
+            return false;
+        }
+
+        $element = &$this->elements;
+        foreach (explode('/', $ukey) as $key) {
+            $element = &$element[$key];
+        }
+        if (isset($element) && (!$override)) {
+            return false;
+        }
+        $element = $value;
+        return true;
+    }
+
+
+    /**
+     * Check if an element with the unique key `$ukey` exists in this collection.
+     *
+     * @param string $ukey Unique element key.
+     *
+     * @return bool True if element existst, false otherwise.
+     *
+     * @since 0.1.0
+     */
+    public function has($ukey)
+    {
+        $element = $this->elements;
+        foreach (explode('/', $ukey) as $key) {
+            if (!isset($element[$key])) {
+                return false;
+            }
+            $element = $element[$key];
+        }
+        return true;
+    }
+
+
+    /**
+     * Returns the count of the FIRST level of keys.
+     *
+     * @return int Count of elements on the first key level.
+     *
+     * @since 0.1.0
+     */
+    public function count()
+    {
+        return count($this->elements);
+    }
+
+
+    /**
+     * Returns the elements in this collection as a JSON-encoded string.
+     *
+     * @return string A JSON representation of this Collection.
+     *
+     * @since 0.1.0
+     * @see http://php.net/manual/de/language.oop5.magic.php#object.tostring Magic __toString()
+     */
+    public function __toString()
+    {
+        return json_encode($this->elements);
+    }
+
+
+    /**
+     * Clear up all elements.
+     *
+     * @return Collection $this - for possible chaining.
+     *
+     * @since 0.1.0
+     */
+    public function clear()
+    {
+        $this->elements = array();
+
+        return $this;
+    }
+
+
+    // NOTE: a real merge cannot be implementet, because we don't know anything
+    // about the type or content of the given elements. This may be done by a
+    // subclass only allowing a specific Object-Type (e.g. with an Interface) of elements.
+
+
+    /**
+     * Merge the elements of another Collection into this Collection.
+     *
+     * If elements have the same unique key on the FIRST level,
+     * they will get replaced with elements of the given Collection `$collection`.
+     * Therefore, all elements on the SECOND level will also be removed, if
+     * there is a duplicate key on the FIRST level.
+     *
+     * @param Collection $collection A valid instance of Collection.
+     *
+     * @return Collection $this - for possible chaining.
+     *
+     * @since 0.1.0
+     */
+    public function fastMerge(Collection $collection)
+    {
+        if (isset($this->elements) && isset($collection->get())) {
+            $this->ellements = array_merge($this->elements, $collection->get());
+        }
+
+        return $this;
+    }
+
+
+    /**
+    * Split a subpart of elements identified by `$ukey` into a seperate Collection object.
+    *
+    * @param string $ukey Unique configuration option key
+    * @return Collection A new Collection object, initialized with found elements for `$ukey`.
+    */
+    public function split($ukey)
+    {
+        return new static($this->get($ukey));
     }
 }
--- tests/Kuetemeier/Collection/CollectionTest.php
+++ tests/Kuetemeier/Collection/CollectionTest.php
@@ -7,34 +7,183 @@ use PHPUnit\Framework\TestCase;
 
 final class CollectionTest extends TestCase
 {
+
+    const TEST_ARRAY = array(
+        'one' => '1',
+        'two' => '2',
+        'three' => array(
+            'A-one' => '31',
+            'B-two' => '32',
+            'C-three' => array (
+                'string' => 'A string',
+                'int' => 10,
+                'bool' => false,
+                'null' => null
+            )
+        )
+
+    );
+
+
     public function testCanBeCreated(): void
     {
+        $c = new Collection();
+
         $this->assertInstanceOf(
             Collection::class,
-            new Collection()
+            $c
         );
+
+        $this->assertEquals(0, $c->count());
     }
 
-    public function testOutput(): void
-    {
-        $this->expectOutputString('Hallo Welt - Hello World!');
-        $collection = new Collection();
-        $collection->helloWorld();
-    }
-/*
-    public function testCannotBeCreatedFromInvalidEmailAddress(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
 
-        Email::fromString('invalid');
+    public function testConstructWithArray(): void
+    {
+        $c = new Collection(self::TEST_ARRAY);
+
+        // return first level element
+        $this->assertEquals('1', $c->get('one'));
+        // return second level element
+        $this->assertEquals('31', $c->get('three/A-one'));
+        // non existing first level key
+        $this->assertEquals(null, $c->get('none'));
+        // non existing secont level key
+        $this->assertEquals(null, $c->get('not/in/array'));
+        // no key
+        $this->assertEquals(self::TEST_ARRAY, $c->get());
+
+        $this->assertEquals(count(self::TEST_ARRAY), $c->count());
     }
 
-    public function testCanBeUsedAsString(): void
+
+    public function testElementsAreNotMutable(): void
     {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
-        );
+        $c = new Collection(self::TEST_ARRAY);
+
+        $e = $c->get('three');
+
+        $this->assertEquals('31', $e['A-one']);
+
+        $e['A-one'] = 'Test';
+
+        $this->assertEquals('Test', $e['A-one']);
+        $this->assertEquals('31', $c->get('three/A-one'));
+
+        $this->assertEquals(count(self::TEST_ARRAY), $c->count());
     }
-*/
+
+
+    public function testRefElementsAreMutable(): void
+    {
+
+        // init with a const
+        $c = new Collection(self::TEST_ARRAY);
+
+        // get a reference of an element
+        $e = &$c->getRef('three');
+
+        // check if we get the right one
+        $this->assertEquals('31', $e['A-one']);
+
+        // manipultate element
+        $e['A-one'] = 'Test';
+
+        // get another reference to the complete elements array
+        $n = &$c->getRef();
+
+        // manipulate it
+        $n['four'] = 'Test 4';
+        $n['three']['B-two'] = 'Test B2';
+
+        // tests
+        $this->assertEquals('Test', $e['A-one']);
+        $this->assertEquals('Test', $c->get('three/A-one'));
+        // 'has' finds the new element
+        $this->assertEquals(true, $c->has('four'));
+        // and it has the right value
+        $this->assertEquals('Test 4', $c->get('four'));
+        $this->assertEquals('Test B2', $c->get('three/B-two'));
+
+        $this->assertEquals(true, $c->has('four'));
+
+        $this->assertEquals(count(self::TEST_ARRAY)+1, $c->count());
+    }
+
+
+    public function testHas(): void
+    {
+        // init with a const
+        $c = new Collection();
+
+        $this->assertEquals(false, $c->has('one'));
+
+
+        // init with a const
+        $c = new Collection(self::TEST_ARRAY);
+
+        $this->assertEquals(true, $c->has('one'));
+        $this->assertEquals(true, $c->has('three/B-two'));
+    }
+
+
+    public function testSet(): void
+    {
+        // test with an empty collection
+        $c = new Collection();
+
+        $this->assertEquals(false, $c->has('first'));
+
+        $v = 'test';
+        $c->set('first', $v);
+
+        $this->assertEquals(true, $c->has('first'));
+        $this->assertEquals($v, $c->get('first'));
+
+        $v = array('1' => 'one', '2' => 'two');
+        $c->set('second', $v);
+
+        $this->assertEquals(true, $c->has('second'));
+        $this->assertEquals($v, $c->get('second'));
+
+        // test with an initialized collection
+        $c = new Collection(self::TEST_ARRAY);
+
+        $this->assertEquals(false, $c->has('first'));
+
+        $v = 'test';
+        $c->set('first', $v);
+
+        $this->assertEquals(true, $c->has('first'));
+        $this->assertEquals($v, $c->get('first'));
+
+        $v = array('1' => 'one', '2' => 'two');
+        $c->set('second', $v);
+
+        $this->assertEquals(true, $c->has('second'));
+        $this->assertEquals($v, $c->get('second'));
+    }
+
+
+    public function testToString(): void
+    {
+        $c = new Collection(self::TEST_ARRAY);
+
+        $s = '' . $c;
+
+        $this->assertEquals(json_encode(self::TEST_ARRAY), $s);
+    }
+
+    public function testClear(): void
+    {
+        $c = new Collection(self::TEST_ARRAY);
+
+        $this->assertEquals(count(self::TEST_ARRAY), $c->count());
+
+        $c->clear();
+
+        $this->assertEquals(0, $c->count());
+        $this->assertEquals(array(), $c->get());
+    }
+
 }
